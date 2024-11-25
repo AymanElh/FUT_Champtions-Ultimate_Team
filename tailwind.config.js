ef41990e1452dc4591619e69a960ea0fc7ef6a8f
AymanElh <elhamioui1964@gmail.com>
2024-11-25 23:48:49 +0100

    make a preview desing with html and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.html", "./assets/**/*.{html, css, js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'stadium': "url('../img/stadium_background.webp')"
+      },
+      screens: {
+        'max-900': {max: '900px'},
+      }
+    },
   },
   plugins: [],
 }
