0239d9d471a3f877e8594f09f479b8df0a28d11d
AymanElh <elhamioui1964@gmail.com>
2024-11-28 15:57:50 +0100

    Add the player information and stats to the array

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,14 +16,9 @@ function changeFormation(formation) {
     squad.classList.add("formation442")
   }
 }
-console.log(formation.value);
+// console.log(formation.value);
 
-// addBtn.addEventListener("click", () => {
-//   changeFormation(formation.value);
-// })
-
-// changeFormation(formation.value);
-
+let currentBtn = null;
 
 // Show the right form for the user
 const plusBtns = document.querySelectorAll(".plus-btn");
@@ -31,6 +26,7 @@ const plusBtns = document.querySelectorAll(".plus-btn");
 plusBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const type = btn.getAttribute("data-type");
+    currentBtn = btn;
 
     document.getElementById("gk-statistics").classList.add("hidden");
     document.getElementById("gk-statistics").classList.remove("flex");
@@ -45,14 +41,95 @@ plusBtns.forEach((btn) => {
     } else {
       document.getElementById("player-statistics").classList.replace("hidden", "flex");
     }
+
+
   });
 });
 
+document.getElementById("add-player").addEventListener("click", () => {
+  if(currentBtn) {
+    addPlayer(currentBtn);
+  }
+  // console.log(players)
+})
 
-function addPlayer() {
+
+function addPlayer(btn) {
+  console.log(btn.parentElement.parentElement.classList[1])
+  let type = btn.getAttribute("data-type");
+
+
+  const playerName = document.getElementById("name-input").value.trim();
+  const playerPhoto = document.getElementById("photo-input").files[0]?.name || "";
+  const playerNationality = document.getElementById("nationality-input").value.trim();
+  const flag = document.getElementById("flag-input").files[0]?.name || "";
+  const playerClub = document.getElementById("club-name-input").value.trim();
+  const clubLogo = document.getElementById("club-logo-input").files[0]?.name || "";
+  const playerRating = document.getElementById("rating-input").value.trim();
+
+  let newPlayer = {
+    name: playerName,
+    photo: playerPhoto,
+    nationality: playerNationality,
+    flag: flag,
+    club: playerClub,
+    clubLogo: clubLogo,
+    position: type,
+    rating: playerRating,
+    stats: {}
+  }
+
+  if(type === "gk") {
+    newPlayer.stats.diving = document.getElementById("diving-input").value.trim();
+    newPlayer.stats.handling = document.getElementById("handling-input").value.trim();
+    newPlayer.stats.kicking = document.getElementById("kicking-input").value.trim();
+    newPlayer.stats.reflexes = document.getElementById("reflexes-input").value.trim();
+    newPlayer.stats.speed = document.getElementById("speed-input").value.trim();
+    newPlayer.stats.positioning = document.getElementById("positioning-input").value.trim();
+  } else {
+    newPlayer.stats.pace = document.getElementById("pace-input").value.trim();
+    newPlayer.stats.shooting = document.getElementById("shooting-input").value.trim();
+    newPlayer.stats.passing = document.getElementById("passing-input").value.trim();
+    newPlayer.stats.dribbling = document.getElementById("dribbling-input").value.trim();
+    newPlayer.stats.defending = document.getElementById("defending-input").value.trim();
+    newPlayer.stats.physical = document.getElementById("pysical-input").value.trim();
+  }
+
+  // reset inputs
+  resetInputs();
+  document.getElementById("player-form").classList.add("hidden")
+
+  players.push(newPlayer);
+  console.log(players)
+}
+
+function resetInputs() {
+  document.getElementById("name-input").value = "";
+  document.getElementById("photo-input").value = "";
+  document.getElementById("nationality-input").value = "";
+  document.getElementById("flag-input").value = "";
+  document.getElementById("club-name-input").value = "";
+  document.getElementById("club-logo-input").value = "";
+  document.getElementById("rating-input").value = "";
+  document.getElementById("diving-input").value = "";
+  document.getElementById("handling-input").value = "";
+  document.getElementById("kicking-input").value = "";
+  document.getElementById("reflexes-input").value = "";
+  document.getElementById("speed-input").value = "";
+  document.getElementById("positioning-input").value = "";
+  document.getElementById("pace-input").value = "";
+  document.getElementById("shooting-input").value = "";
+  document.getElementById("passing-input").value = "";
+  document.getElementById("dribbling-input").value = "";
+  document.getElementById("defending-input").value = "";
+  document.getElementById("pysical-input").value = "";
+}
+
+function addPlayerToCard(player) {
   
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+
+document.getElementById("formation-select").addEventListener("change", () => {
     changeFormation(formation.value)
 })
